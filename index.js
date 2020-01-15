7dd2bf7a5c3a1a83ea2891f86f62f89b263b7541
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-01-15 00:00:47 -0300

    Database connection

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0-6e8hf.mongodb.net/DevRadar?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(express.json());
 
