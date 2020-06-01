936a597436942b1937713f7be1525446b73a421d
Mateus Henrique <mateushenriquechaves1@gmail.com>
2020-06-01 15:15:58 -0300

    cors fix

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const cors = require('cors');
 
 require('dotenv').config();
 
+
+
 const routes = require('./routes');
 
 const app = express();
+app.use(cors());
 
 mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0-6e8hf.mongodb.net/DevRadar?retryWrites=true&w=majority`, {
     useNewUrlParser: true,
@@ -15,7 +18,6 @@ mongoose.connect(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATAB
 
 app.use(express.json());
 app.use(routes);
-app.use(cors());
 
 app.listen(
     process.env.PORT,
